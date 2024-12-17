d86790b072c632e9304c9c740fee707708cbcd4c
Paul Kindler <pa179kin@gmail.com>
2024-12-17 18:00:37 +0100

    changed font to Montserrat
    Signed-off-by: Paul Kindler <pa179kin@gmail.com>

--- app/src/commonMain/kotlin/fyi/pauli/trak/ui/theme/Theme.kt
+++ app/src/commonMain/kotlin/fyi/pauli/trak/ui/theme/Theme.kt
@@ -1,7 +1,12 @@
 package fyi.pauli.trak.ui.theme
 
-enum class Theme {
-  YOU,
-  MENSTRUATION,
-  TOGETHER
+import androidx.compose.material3.ColorScheme
+import fyi.pauli.trak.ui.theme.period.PeriodTheme
+import fyi.pauli.trak.ui.theme.together.TogetherTheme
+import fyi.pauli.trak.ui.theme.you.YouTheme
+
+enum class Theme(val lightScheme: ColorScheme, val darkScheme: ColorScheme) {
+  YOU(YouTheme.lightScheme, YouTheme.darkScheme),
+  MENSTRUATION(PeriodTheme.lightScheme, PeriodTheme.darkScheme),
+  TOGETHER(TogetherTheme.lightScheme, TogetherTheme.darkScheme)
 }
