b213ed0949f8809e14c432d3439f4f33e6801823
Paul K <pa179kin@gmail.com>
2025-09-04 13:57:06 +0200

    use buildSrc instead
    Signed-off-by: Paul K <pa179kin@gmail.com>

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    fun plugin(id: String, version: String) = "$id:$id.gradle.plugin:$version"
+
+    val kotlinVersion = "2.2.10"
+    val androidVersion = "8.11.1"
+
+    implementation(kotlin("gradle-plugin", kotlinVersion))
+
+    compileOnly(plugin("org.jetbrains.kotlin.plugin.serialization", embeddedKotlinVersion))
+    runtimeOnly(plugin("org.jetbrains.kotlin.plugin.serialization", kotlinVersion))
+
+    implementation(plugin("com.android.library", androidVersion))
+    implementation(plugin("com.android.application", androidVersion))
+
+    implementation(plugin("com.android.kotlin.multiplatform.library", androidVersion))
+
+    implementation(plugin("com.google.devtools.ksp", "2.1.0-1.0.29"))
+
+    implementation(plugin("org.jetbrains.compose", "1.8.2"))
+    implementation(plugin("org.jetbrains.kotlin.plugin.compose", kotlinVersion))
+
+    implementation(plugin("app.cash.sqldelight", "2.1.0"))
+
+    implementation(plugin("io.ktor.plugin", "3.2.3"))
+}
---
+++ buildSrc/src/main/kotlin/multiplatform-project.gradle.kts
@@ -0,0 +1,104 @@
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import com.android.build.api.dsl.androidLibrary
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+
+plugins {
+    kotlin("multiplatform")
+    id("com.android.kotlin.multiplatform.library")
+}
+
+kotlin {
+    androidLibrary {
+        namespace = "${project.group}.trak"
+        compileSdk = 36
+        minSdk = 24
+
+        withJava()
+
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_1_8
+        }
+    }
+
+    explicitApi()
+
+    compilerOptions {
+        freeCompilerArgs.add("-Xexpect-actual-classes")
+
+        apiVersion = KotlinVersion.KOTLIN_2_2
+        languageVersion = KotlinVersion.KOTLIN_2_2
+    }
+
+    jvm()
+
+    linuxX64()
+    linuxArm64()
+
+    mingwX64()
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "trak"
+            isStatic = true
+        }
+    }
+
+    macosX64()
+    macosArm64()
+
+    wasmJs("web") {
+        outputModuleName = "trak"
+        browser {
+            commonWebpackConfig {
+                outputFileName = "trak.js"
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+                    static = (static ?: mutableListOf(
+                        project.rootDir.path,
+                        project.project.path
+                    ))
+                }
+            }
+        }
+        binaries.executable()
+    }
+
+    applyDefaultHierarchyTemplate()
+
+    sourceSets {
+        all {
+            languageSettings.optIn("kotlin.uuid.ExperimentalUuidApi")
+        }
+
+        commonMain {
+            dependencies {
+                implementation("io.github.oshai:kotlin-logging:7.0.3")
+            }
+        }
+
+        jvmMain {
+            dependencies {
+                runtimeOnly("ch.qos.logback:logback-classic:1.5.18")
+            }
+        }
+
+        commonTest {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        jvmTest.dependencies {
+            implementation("org.junit.jupiter:junit-jupiter-engine:5.13.4")
+        }
+    }
+}
