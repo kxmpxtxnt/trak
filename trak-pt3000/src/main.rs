38e0e72af3d105b652c1e8ee6d59552d7491ad82
Paul K <pa179kin@gmail.com>
2025-11-28 16:07:28 +0100

    add random led blinking
    Signed-off-by: Paul K <pa179kin@gmail.com>

--- trak-pt3000/src/main.rs
+++ trak-pt3000/src/main.rs
@@ -12,10 +12,11 @@ use core::panic::PanicInfo;
 use embedded_dht_rs::dht11::Dht11;
 use esp_hal::delay::Delay;
 use esp_hal::gpio::{DriveMode, Level, Output, OutputConfig};
+use esp_hal::rng::Rng;
 use esp_hal::{clock::CpuClock, main, rmt::Rmt, time::Rate};
 use esp_hal_smartled::{smart_led_buffer, SmartLedsAdapter};
 use esp_println::println;
-use smart_leds::RGB8;
+use smart_leds::{SmartLedsWrite, RGB8};
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
@@ -28,11 +29,7 @@ fn main() -> ! {
     let mut buffer = smart_led_buffer!(1);
     let mut led = SmartLedsAdapter::new(rmt.channel0, peripherals.GPIO8, &mut buffer);
 
-    const LEVEL: u8 = 0;
-    let mut color = RGB8::default();
-    color.r = LEVEL;
-
-    let delay = Delay::new();
+    let mut color = RGB8::new(255, 255, 255);
 
     let mut output = Output::new(
         peripherals.GPIO2,
@@ -43,9 +40,13 @@ fn main() -> ! {
 
     output.set_input_enable(true);
     output.set_output_enable(true);
-    let mut dht11 = Dht11::new(output, delay);
 
+    let delay = Delay::new();
+
+    let mut dht11 = Dht11::new(output, delay);
     let mut hcsr04 = HcSr04::new(peripherals.GPIO5, peripherals.GPIO4, delay);
+
+    let rng = Rng::new();
 
     loop {
         delay.delay_millis(500);
@@ -58,7 +59,7 @@ fn main() -> ! {
             }
         };
 
-        let distance = hcsr04.measure(temp);
+        let measurement = hcsr04.measure(temp);
 
         match hcsr04.measure(temp) {
             Ok((distance, temperature)) => {
@@ -66,6 +67,9 @@ fn main() -> ! {
             }
             Err(err) => println!("Error while measuring distance: {:?}.", err),
         }
+
+        color = RGB8::new(rng.random() as u8, rng.random() as u8, rng.random() as u8);
+        led.write([color].into_iter()).unwrap();
     }
 }
 
